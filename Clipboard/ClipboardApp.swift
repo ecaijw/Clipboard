13560f00a86a2429cd4dd3d9f2730cb1c3d32796
luke <luke@d7d.com.au>
2024-07-30 10:58:52 +1000

    Initial Commit

---
+++ Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by Luke Cai on 30/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
