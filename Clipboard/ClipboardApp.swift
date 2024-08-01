b783c4b8bd0542565d2f2042026bda6916372582
luke <luke@d7d.com.au>
2024-08-01 10:59:09 +1000

    add shortcut key, history

--- Clipboard/ClipboardApp.swift
+++ Clipboard/ClipboardApp.swift
@@ -5,26 +5,66 @@
 //  Created by Luke Cai on 30/7/2024.
 //
 
-import Cocoa
 import SwiftUI
+import MASShortcut
 
 @main
 struct ClipboardApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    var popover = NSPopover()
+    var popoverTransiencyMonitor: Any?
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Perform any setup after launching the application.
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem?.button {
+            button.image = NSImage(systemSymbolName: "doc.on.clipboard", accessibilityDescription: "Clipboard")
+            button.action = #selector(togglePopover(_:))
+        }
+        
+        popover.contentViewController = NSHostingController(rootView: ContentView())
+        popover.behavior = .transient
+
+        // Monitor for clicks outside of the popover to close it
+        popoverTransiencyMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+            self?.popover.performClose(event)
+        }
+
+        // Register global shortcut (Cmd + Shift + V)
+        let shortcut = MASShortcut(keyCode: kVK_ANSI_V, modifierFlags: [.command, .shift])
+        MASShortcutMonitor.shared().register(shortcut, withAction: { [weak self] in
+            self?.togglePopover(nil)
+        })
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = statusItem?.button {
+            if popover.isShown {
+                popover.performClose(sender)
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .maxY)
+                adjustPopoverPosition()
+                NSApp.activate(ignoringOtherApps: true)
+            }
+        }
     }
 
-    func applicationWillTerminate(_ notification: Notification) {
-        // Perform any cleanup before terminating the application.
+    func adjustPopoverPosition() {
+        guard let popoverWindow = popover.contentViewController?.view.window else {
+            return
+        }
+
+        var popoverFrame = popoverWindow.frame
+        popoverFrame.origin.y -= 150  // Move the popover down by 150 pixels
+        popoverWindow.setFrame(popoverFrame, display: true, animate: true)
     }
 }
--- Clipboard/ClipboardManager.swift
+++ Clipboard/ClipboardManager.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Luke Cai on 30/7/2024.
 //
-
 import AppKit
 import Combine
 
@@ -12,16 +11,31 @@ class ClipboardManager: ObservableObject {
     @Published var history: [String] = []
     private var timer: Timer?
     private var lastChangeCount: Int = 0
+    private var previousFocusedApp: NSRunningApplication?
+    private let historyFileURL: URL
 
     init() {
+        let fileManager = FileManager.default
+        let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+        let appDirectory = appSupportURL.appendingPathComponent("ClipboardApp")
+
+        if !fileManager.fileExists(atPath: appDirectory.path) {
+            try? fileManager.createDirectory(at: appDirectory, withIntermediateDirectories: true, attributes: nil)
+        }
+
+        self.historyFileURL = appDirectory.appendingPathComponent("clipboard_history.txt")
+
+        loadHistory()
         startMonitoring()
     }
 
     func startMonitoring() {
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(checkClipboard), userInfo: nil, repeats: true)
+        previousFocusedApp = NSWorkspace.shared.frontmostApplication
     }
 
     @objc private func checkClipboard() {
+        previousFocusedApp = NSWorkspace.shared.frontmostApplication
         if let clipboardContent = NSPasteboard.general.string(forType: .string) {
             if NSPasteboard.general.changeCount != lastChangeCount {
                 lastChangeCount = NSPasteboard.general.changeCount
@@ -33,11 +47,46 @@ class ClipboardManager: ObservableObject {
     private func addClipboardContent(_ content: String) {
         if !history.contains(content) {
             history.append(content)
+            saveHistory()
+        }
+    }
+
+    private func saveHistory() {
+        let historyString = history.joined(separator: "\n")
+        try? historyString.write(to: historyFileURL, atomically: true, encoding: .utf8)
+    }
+
+    private func loadHistory() {
+        if let historyString = try? String(contentsOf: historyFileURL, encoding: .utf8) {
+            self.history = historyString.components(separatedBy: "\n").filter { !$0.isEmpty }
         }
     }
 
     func pasteToClipboard(content: String) {
         NSPasteboard.general.clearContents()
         NSPasteboard.general.setString(content, forType: .string)
+        pasteToPreviousApp()
+    }
+
+    private func pasteToPreviousApp() {
+        // TODO
+        return
+        guard let app = previousFocusedApp else { return }
+
+        let source = """
+        tell application "\(app.bundleIdentifier!)"
+            activate
+            tell application "System Events" to keystroke "v" using command down
+        end tell
+        """
+
+        var error: NSDictionary?
+        if let scriptObject = NSAppleScript(source: source) {
+            scriptObject.executeAndReturnError(&error)
+        }
+
+        if let error = error {
+            print("Error: \(error)")
+        }
     }
 }
